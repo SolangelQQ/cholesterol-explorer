77d2c9b5b7178b8e1a081f74a92c08ecb0ba6a2b
ricardo051199 <jesus.pelaez@ucb.edu.bo>
2024-03-06 13:24:00 -0400

    UI and BE: Prototype app.(add two numbers)

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,19 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return jsonify(message="¡Hola desde el backend en Python!")
+
+@app.route('/sumar', methods=['POST'])
+def sumar():
+    try:
+        data = request.get_json()
+        num1 = float(data.get('num1', 0))  # Si 'num1' no está presente, establecerlo a 0
+        num2 = float(data.get('num2', 0))  # Si 'num2' no está presente, establecerlo a 0
+        resultado = num1 + num2
+        return jsonify(resultado=resultado)
+    except ValueError:
+        return jsonify(error="Los datos proporcionados no son números válidos"), 400
+
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:5000/', { mode: 'cors' })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('message').innerText = data.message;
-        })
-        .catch(error => console.error('Error al conectar con el backend:', error));
+    const calculatorForm = document.getElementById('calculatorForm');
+
+    calculatorForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const num1 = parseFloat(document.getElementById('num1').value);
+        const num2 = parseFloat(document.getElementById('num2').value);
+
+        console.log("num1:", num1);
+        console.log("num2:", num2);
+
+        sumar(num1, num2);
+    });
 });
+
+function sumar(num1, num2) {
+    fetch('http://localhost:5000/sumar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ num1, num2 }),
+        mode: 'cors',
+        credentials: 'same-origin',
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').innerText = `Resultado: ${data.resultado}`;
+    })
+    .catch(error => console.error('Error al conectar con el backend:', error));
+}
